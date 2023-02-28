47fcbc62bca527e8b7205d743ec819b390c6376c
Mainak Dev <mainak@mainak.local>
2023-02-28 09:13:56 +0530

    feb 28 final commit

--- 28_02_2023/app.js
+++ 28_02_2023/app.js
@@ -9,8 +9,9 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- 28_02_2023/routes/shop.js
+++ 28_02_2023/routes/shop.js
@@ -3,11 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminRoutes = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log(adminRoutes.product);
 });
 
 module.exports = router;
