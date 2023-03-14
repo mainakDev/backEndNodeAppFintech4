0f9f5d47b887c28e8b276a437a1fc26601ed1fa9
Mainak Dev <mainak@192.168.1.5>
2023-03-14 12:12:44 +0530

    added MVC starter files

--- MVC/routes/admin.js
+++ MVC/routes/admin.js
@@ -6,34 +6,24 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const product = [];
+const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  // console.log(req.body);
-  // console.log(req.body.title);
-  product.push(req.body.title);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
 exports.routes = router;
-exports.product = product;
-
-
-exportedObject = {
-  routes: router,
-  product: product
-}
-
-person = {
-  name: 'XYZ',
-  phone: '123'
-
-}
-
-person.phone
+exports.products = products;
