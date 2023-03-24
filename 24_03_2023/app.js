7dc7c2699c0038e12d9f463a5cfa18a0c07d6a9e
Mainak Dev <mainakonandroid@gmail.com>
2023-03-24 10:27:36 +0530

    added 24th March folder

---
+++ 24_03_2023/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page not found'});
+});
+
+app.listen(3000);
